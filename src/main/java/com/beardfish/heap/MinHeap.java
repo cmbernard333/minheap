d436fb4ba4e436ffa566315b4891153fe1aa002a
Christian Bernard <masterblah777@gmail.com>
2014-02-23 22:08:56 -0700

    --MinHeap.java added clear method. added indexOf, remove, removeAt, and percolateDown methods. These methods are unfinished. Added getSmallerChild methods, but these are not totally defined yet.

--- src/main/java/com/beardfish/heap/MinHeap.java
+++ src/main/java/com/beardfish/heap/MinHeap.java
@@ -197,6 +197,101 @@ public class MinHeap<E> extends AbstractQueue<E> {
 			this.queue[index] = element;
 		}
 	}
+
+	
+	/**
+	 * Find the object in the queue 
+	 * @param o
+	 * @return 0 if found; -1 otherwise
+	 */
+	private int indexOf(Object o) {
+		if(o!=null) {
+			for(int i = 0;i<this.size;i++) {
+				if(o.equals(this.queue[i])) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * Remove the particular object if it exists
+	 * @param o - the object to remove
+	 * @return true if it exists; false otherwise
+	 */
+	public boolean remove(Object o) {
+		int index = indexOf(o);
+		if(index!=-1) {
+			removeAt(index);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Remove the object at the specified index
+	 * @param index
+	 * @return the object removed
+	 */
+	public E removeAt(int index) {
+		assert index >= 0 && index < size;
+		this.modCount++;
+		int s = --size;
+		E element = (E) this.queue[index];
+		/* last element */
+		if(s==index) {
+			this.queue[index] = null;
+		} else {
+			/* place the last element at the index of the removed element */
+			this.queue[index] = this.queue[s];
+			this.queue[s] = null;
+			this.percolateDown(index,(E) this.queue[index]);
+		}
+		return element;
+		
+	}
+
+	
+	/**
+	 * percolateDown until you find that both children are less than the element
+	 * while percolating you should replace with the smaller of the children to maintain
+	 * the heap property
+	 * @param index
+	 * @param element
+	 */
+	private void percolateDown(int index, E element) {
+		if(this.comparator!=null) {
+			this.percolateDownWithComparator(index, element);
+		} else {
+			this.percolateDownComparable(index, element);
+		}
+	}
+	
+	/**
+	 * Percolate down using the comparator was provided at construction time
+	 * @param index
+	 * @param element
+	 */
+	private void percolateDownWithComparator(int index, E element) {
+		if(index==this.size) {
+			return;
+		}
+		
+		
+	}
+	
+	/**
+	 * Percolate down by casting an element to the Comparable of the appropriate type
+	 * @param index
+	 * @param element
+	 */
+	private void percolateDownComparable(int index, E element) {
+		if(index==this.size) {
+			return;
+		}
+		Comparable<? super E> key = (Comparable<? super E>) element;
+	}
 	
 	/**
 	 * Retrieve the parent index of a given item
@@ -214,16 +309,64 @@ public class MinHeap<E> extends AbstractQueue<E> {
 		return p;
 	}
 	
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return new Itr();
+	/**
+	 * Calculates the children for a given index
+	 * Checks to see which one is smaller and returns the index
+	 * It will return the index of a null item if its at the end
+	 * @param index
+	 * @return index of smaller child
+	 */
+	private int getSmallerChildComparator(int index) {
+		int c = 0;
+		int leftChild = (2*index)+1;
+		int rightChild = (2*index)+2;
+		if(this.queue[rightChild]==null) {
+			c = leftChild;
+		} else {
+			if(this.comparator.compare((E)this.queue[leftChild],(E)this.queue[rightChild])<0) {
+				c = leftChild;
+			} else {
+				c = rightChild;
+			}
+		}
+		return c;
 	}
+	
+	private int getSmallerChildComparable(int index) {
+		int c = 0;
+		int leftChild = (2*index)+1;
+		int rightChild = (2*index)+2;
+		if(this.queue[rightChild]==null) {
+			c = leftChild;
+		} else {
+			Comparable<? super E> key = (Comparable<? super E>) this.queue[leftChild];
+			if(key.compareTo((E)this.queue[rightChild])<0) {
+				c = leftChild;
+			} else {
+				c = rightChild;
+			}
+		}
+		return c;
+	}
+	
 	
 	@Override
 	public String toString() {
 		return Arrays.toString(this.queue);
+	}
+	
+	public void clear() {
+		modCount++;
+		for(int i = 0; i< this.size; i++) {
+			this.queue[i] = null;
+		}
+		this.size=0;
+	}
+	
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return new Itr();
 	}
 	
 	private final class Itr implements Iterator<E> {
